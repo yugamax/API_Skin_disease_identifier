5ad03c3c1adf0bb5e5d1eaeb21d72e944cd40df8
yugamax <122220846+yugamax@users.noreply.github.com>
2025-04-06 12:42:59 +0530

    Update API_skin_disease.py

--- API_skin_disease.py
+++ API_skin_disease.py
@@ -44,7 +44,10 @@ async def predict(file: UploadFile = File(...)):
         pred_conf = pred[0][pred_ind]
         
         if float(pred_conf) < 0.6:
-            return {"predicted_class": class_list[3], "confidence": (float(1)-float(pred_conf)) * 100}
+            if float(pred_conf) < 0.5:
+                return {"predicted_class": class_list[3], "confidence": (float(1)-float(pred_conf)) * 100}
+            else:
+                return {"predicted_class": class_list[3], "confidence": float(pred_conf) * 100}
         else:
             return {"predicted_class": class_list[pred_ind], "confidence": float(pred_conf) * 100}
                 
